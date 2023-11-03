ad891fbfa0628e6e479ebad83188a77c93d16e46
idunne360 <imdunne360@gmail.com>
2023-11-03 01:22:33 -0400

    devise fully functional; logout + some tests added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(email: 'test@example.com', password: 'password', password_confirmation: 'password') }
+  
+  describe 'database_authenticatable' do
+    it 'authenticates with correct password' do
+      expect(user.valid_password?('password')).to be_truthy
+    end
+
+    it 'does not authenticate with incorrect password' do
+      expect(user.valid_password?('wrong_password')).to be_falsey
+    end
+  end
+
+  #needs actual host not localhost
+  # describe 'recoverable' do
+  #   it 'generates a reset password token' do
+  #     user.send_reset_password_instructions
+  #     expect(user.reset_password_token).not_to be_nil
+  #   end
+  # end
+
+  describe 'validatable' do
+    it 'validates presence of email' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'validates format of email' do
+      user.email = 'invalid_email'
+      expect(user).not_to be_valid
+    end
+
+    it 'validates presence of password' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'validates confirmation of password' do
+      user.password_confirmation = 'wrong_password'
+      expect(user).not_to be_valid
+    end
+  end
 end
