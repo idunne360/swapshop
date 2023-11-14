8c5de273104ca80484ab4f40df965471fdbf3539
Lucca-Figlioli <lfiglioli@colgate.edu>
2023-11-13 23:01:51 -0500

    Cleanup

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
     validates :title, presence: true
     validates :description, presence: true
